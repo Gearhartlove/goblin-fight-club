f1bf39014d733f4928737a2d1e225ccc42a18c8b
kristoff <kristoffgearhartfinley@gmail.com>
2024-11-01 08:25:52 -0600

    progress

---
+++ lib/goblin_fight_club/setup.ex
@@ -0,0 +1,27 @@
+defmodule GoblinFightClub.Setup do
+  alias GoblinFightClub.{Monster, Repo}
+
+  @doc """
+  Add's the monster's to the database
+  """
+  def seed() do
+    all_monsters =
+      Path.wildcard("priv/static/assets/pathfinder-bestiary/*.json")
+      |> Enum.map(&File.read!(&1))
+      |> IO.inspect(label: "After reading file")
+      |> Enum.map(&Jason.decode!(&1))
+      |> IO.inspect(label: "After decoding json")
+      |> Enum.map(
+        &%{name: get_in(&1, ["name"]), level: get_in(&1, ["system", "details", "level", "value"])}
+      )
+      |> IO.inspect(label: "After grabbing data")
+
+    Enum.each(all_monsters, fn monster ->
+      %Monster{}
+      |> Monster.changeset(monster)
+      |> Repo.insert!()
+    end)
+
+    IO.puts("Fin")
+  end
+end
